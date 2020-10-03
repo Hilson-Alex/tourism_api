e102231ea40fac6d57d4a3f20a106d25cba07e5a
Hilson Alexandre <hilson.alex234@gmail.com>
2020-10-03 10:41:08 -0300

    Começando a rotear o backend

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 const app = express();
 app.use(cors());
+app.use(express.json);
+
+async function getAll (){
+    let result = [];
+    try {
+        await client.connect();
+        const collection = client.db("tourism").collection("cases");
+        result = await collection.find({}).toArray();
+    } finally {
+        await client.close();
+    }
+    return result;
+
+}
+
+app.all("/", (req, res) => {
+    res.status(200).json(getAll());
+})
 
 app.listen(process.env.PORT || 8080)
 
