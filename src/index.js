addfe20795020fbd7292e60d96de966846e64412
Hilson Alexandre <hilson.alex234@gmail.com>
2020-10-04 23:14:27 -0300

    update

--- src/index.js
+++ src/index.js
@@ -11,32 +11,28 @@ app.use(cors());
 app.set("port", process.env.PORT || 8080);
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 // middleware to send response
 async function send (req, res) {
     res.json(req.array).status(200);
 }
 
-function allow (req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', '*'); // or something like http://localhost:3000'
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // To expose all headers in all responses
-    res.setHeader('Access-Control-Expose-Headers', '*');
-    next();
-}
 
 // get all cases
-app.all('/', allow, db.getAll, send);
+app.all('/', db.getAll, send);
 
 // get solutions by priority
-app.post("/similarity", allow, db.getAll, similarity.getSimilarity, similarity.sortResults, send);
+app.post("/similarity", db.getAll, similarity.getSimilarity, similarity.sortResults, send);
 
 // insert a case
-app.post("/insert", allow, db.insert);
+app.options("/insert", cors());
+app.post("/insert", db.insert);
 
 app.listen(process.env.PORT || 8080);
